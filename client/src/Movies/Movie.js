1febf9fdd3183f9a591d2f432fd4658cc03933b3
CeeDeeBee <cdbarnes98@gmail.com>
2020-03-25 16:02:12 -0400

    Added edit functionality

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,40 +1,47 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { useRouteMatch } from 'react-router-dom';
-import MovieCard from './MovieCard';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useRouteMatch, useHistory } from "react-router-dom";
+import MovieCard from "./MovieCard";
 
 function Movie({ addToSavedList }) {
-  const [movie, setMovie] = useState(null);
-  const match = useRouteMatch();
+	const history = useHistory();
+	const [movie, setMovie] = useState(null);
+	const match = useRouteMatch();
 
-  const fetchMovie = id => {
-    axios
-      .get(`http://localhost:5000/api/movies/${id}`)
-      .then(res => setMovie(res.data))
-      .catch(err => console.log(err.response));
-  };
+	const fetchMovie = id => {
+		axios
+			.get(`http://localhost:5000/api/movies/${id}`)
+			.then(res => setMovie(res.data))
+			.catch(err => console.log(err.response));
+	};
 
-  const saveMovie = () => {
-    addToSavedList(movie);
-  };
+	const saveMovie = () => {
+		addToSavedList(movie);
+	};
 
-  useEffect(() => {
-    fetchMovie(match.params.id);
-  }, [match.params.id]);
+	useEffect(() => {
+		fetchMovie(match.params.id);
+	}, [match.params.id]);
 
-  if (!movie) {
-    return <div>Loading movie information...</div>;
-  }
+	if (!movie) {
+		return <div>Loading movie information...</div>;
+	}
 
-  return (
-    <div className='save-wrapper'>
-      <MovieCard movie={movie} />
+	return (
+		<div className="save-wrapper">
+			<MovieCard movie={movie} />
 
-      <div className='save-button' onClick={saveMovie}>
-        Save
-      </div>
-    </div>
-  );
+			<div className="save-button" onClick={saveMovie}>
+				Save
+			</div>
+			<div
+				className="edit-button"
+				onClick={() => history.push(`/update-movie/${match.params.id}`)}
+			>
+				Edit
+			</div>
+		</div>
+	);
 }
 
 export default Movie;
